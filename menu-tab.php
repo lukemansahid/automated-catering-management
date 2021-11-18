b5f1058d59faa57ea728e6a1e88d1eab816dbefe
lukemansahid <lukemansahid2020@gmail.com>
2021-11-18 11:09:11 +0000

    UPDATED: added user functionality

--- menu-tab.php
+++ menu-tab.php
@@ -1,23 +1,62 @@
+
+<div class="navbar navbar-fixed-top bs-docs-nav" role="banner">
+
+    <div class="conjtainer">
+        <!-- Menu button for smallar screens -->
+        <div class="navbar-header">
+            <button class="navbar-toggle btn-navbar" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                <span>Menu</span>
+            </button>
+        </div>
+
+        <!-- Navigation starts -->
+        <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+
+                <li class="dropdown dropdown-big">
+                    <a href="#"><span class = "label label-info"><i class="fa fa-calendar"></i></span> <?php echo date("F d, Y");?></a>
+                </li>
+            </ul>
+
+            <!-- Search form -->
+
+            <!-- Links -->
+            <ul class="nav navbar-nav pull-right">
+                <li class="dropdown pull-right">
+                    <a href="#login" data-toggle="dropdown">
+                        <i class="fa fa-user"></i> <?php
+                        if(!empty($_SESSION['id'])){
+                            echo $_SESSION['username'];
+                        } ?>
+                    </a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu">
+                        <li><a href="admin/logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+
+
+    </div>
+</div>
+
+
 <!-- Header starts -->
   <header>
     <div class="container">
       <div class="row">
-        <!-- Logo section -->
-        <div class="col-md-4">
-          <!-- Logo. -->
-          <div class="logo">
-           <a href="userdashboard.php"> <h2 class = "logo-text"><span class="bold">Automated Catering Management System</span></h2></a>
-           
+          <div class="col-md-3 pull-left">
+              <h2><i class="fa fa-home"></i> Dashboard</h2>
           </div>
-          <!-- Logo ends -->
-        </div>
         <!-- Button section -->
-        <div class="col-md-7">
+        <div class="col-md-7 pull-right">
           <!-- Buttons -->
           <ul class="nav nav-tabs">
 
 			<li class="dropdown dropdown-big">
-              <a  href="userdashboard.php.php"><i class="fa fa-home"></i> Home</a>
+              <a  href="userdashboard.php"><i class="fa fa-home"></i> Home</a>
             </li>
 			
 			
@@ -30,7 +69,7 @@
             </li>
 
             <li class="dropdown dropdown-big">
-                <a href="reservation.php"><i class="fa fa-pencil"></i> Schedule a Reservation</a>
+                <a href="#"><i class="fa fa-pencil"></i> Schedule a Reservation</a>
             </li> 
 
             <li class="dropdown dropdown-big">
@@ -43,11 +82,5 @@
         <!-- Data section -->
       </div>
     </div>
-		<?php include 'index.php'; ?>
-		<?php include 'details_modal.php'; ?>		
-	    <?php include 'reservation_modal.php'; ?>
-	    <?php include 'facilities_modal.php'; ?>
-	    <?php include 'culinary_modal.php'; ?>
-    
   </header>
   
